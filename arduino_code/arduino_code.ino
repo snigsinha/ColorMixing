9caac4cd5e3607feab47328ed14c295789e94222
ShmalTalk <talie_massachi@brown.edu>
2019-11-01 16:10:38 -0400

    complete arduino code

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+int diamondSensorPin = A0; // FSR is connected to analog 0
+int heartSensorPin = A1;
+int starSensorPin = A2;
+int sensorReading;      // the analog reading from the FSR resistor divider
 
+void setup(void) {
+  Serial.begin(9600);   // We'll send debugging information via the Serial monitor
+}
+ 
+void loop(void) {
+  isTouched(diamondSensorPin, 0);
+  isTouched(heartSensorPin, 1);
+  isTouched(starSensorPin, 2);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+bool isTouched(int sensorPin, int serialOutVal) {
+  sensorReading = analogRead(sensorPin);
+  if (sensorReading > 200) {
+    Serial.println(serialOutVal);
+  } 
 }
