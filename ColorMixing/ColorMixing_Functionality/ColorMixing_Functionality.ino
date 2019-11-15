660fe60119d1f745e3fb88864ed7f82c782fb46b
Snigdha Sinha <Snigdha@snigdhasmacbook.devices.brown.edu>
2019-11-15 12:09:26 -0500

    Adding new ColorMixing Files

---
+++ ColorMixing/ColorMixing_Functionality/ColorMixing_Functionality.ino
@@ -0,0 +1,31 @@
+//Tutorial for RBG LED: https://www.youtube.com/watch?v=5Qi93MjlqzE 
+
+Code
+int red_light_pin= 11;
+int green_light_pin = 10;
+int blue_light_pin = 9;
+void setup() {
+  pinMode(red_light_pin, OUTPUT);
+  pinMode(green_light_pin, OUTPUT);
+  pinMode(blue_light_pin, OUTPUT);
+}
+void loop() {
+  RGB_color(255, 0, 0); // Red
+  delay(1000);
+  RGB_color(0, 255, 0); // Green
+  delay(1000);
+  RGB_color(0, 0, 255); // Blue
+  delay(1000);
+  RGB_color(255, 0, 255); // Magenta
+  delay(1000);
+  RGB_color(255, 255, 0); // Yellow
+  delay(1000);
+  RGB_color(255, 255, 255); // White
+  delay(1000);
+}
+void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+ {
+  analogWrite(red_light_pin, red_light_value);
+  analogWrite(green_light_pin, green_light_value);
+  analogWrite(blue_light_pin, blue_light_value);
+}
