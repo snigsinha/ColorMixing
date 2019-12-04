6edae95ac1f3909ef1968057b5e1e0b3a016dc86
Talie Massachi <talie@brown.edu>
2019-12-04 12:10:03 -0500

    RYB 2 RGB (#5)
    * working on RYB2RGB direct
    * sucessful for all primary and secondary colors

--- ColorMixing/ColorMixing_Functionality/ColorMixing_Functionality.ino
+++ ColorMixing/ColorMixing_Functionality/ColorMixing_Functionality.ino
@@ -6,7 +6,8 @@ int pallete_red = A0;
 int pallete_yellow = A1; // FSR is connected to analog 0
 int pallete_blue = A2;
 int sensorReading;  
-int colorCount[3] = {0, 0, 0}; //R, Y, B
+float colorCount[3] = {0, 0, 0}; //R, Y, B
+
 // this is used to make sure the count does not increase if 
 //the user keeps holding a color for longer than a loop
 bool canIncrease[3] = {true, true, true};
@@ -30,43 +31,28 @@ void loop() {
  // at this point we will have the count for each
   RYB_to_RGB();
   RGB_color(r_g_b[0], r_g_b[1], r_g_b[2]); // Red, green, blue
-  Serial.println(colorCount[0]);
-  Serial.println(colorCount[1]);
-  Serial.println(colorCount[2]);
-  delay(300);
+  
+//  Serial.println(colorCount[0]);
+//  Serial.println(colorCount[1]);
+//  Serial.println(colorCount[2]);
+//  
+  Serial.println(r_g_b[0]);
+  Serial.println(r_g_b[1]);
+  Serial.println(r_g_b[2]);
+  delay(100);
 }
 
 void RYB_to_RGB() {
-  int p = min(colorCount[1],colorCount[2]);
-  int r[3] = {0,100,100};
-  int y[3] = {0,0,100};
-  int b[3] = {100,100,0};
-  int g[3] = {0,-100,0};
 
-  int rgb_r[3] = {colorCount[0]*r[0], colorCount[0]*r[1], colorCount[0]*r[2]};
-  int rgb_y[3] = {colorCount[1]*y[0], colorCount[1]*y[1], colorCount[1]*y[2]};
-  int rgb_b[3] = {colorCount[2]*b[0], colorCount[2]*b[1], colorCount[1]*b[2]};
-  int rgb_g[3] = {p*g[0], p*g[1], p*g[2]};
-  
+  int r_steps = colorCount[0] + 4*colorCount[1] - 4*min(colorCount[1],colorCount[2]); // red + yellow - green
+  int g_steps = colorCount[1]; // yellow
+  int b_steps = colorCount[2] - min(colorCount[1],colorCount[2]); // blue - green
 
-  int r_column = rgb_r[0] + rgb_y[0] + rgb_b[0] + rgb_g[0];
-  int g_column = rgb_r[1] + rgb_y[1] + rgb_b[1] + rgb_g[1];
-  int b_column = rgb_r[2] + rgb_y[2] + rgb_b[2] + rgb_g[2];
+  float max_steps = max(r_steps, max(g_steps, b_steps))*1.0;
 
-  int max_ratio = max(r_column, max(g_column, b_column))/100;
-  
-  r_column = r_column/max_ratio;
-  g_column = g_column/max_ratio;
-  b_column = b_column/max_ratio;
-  
-  int r_final = 255*((100- r_column)/100);
-  int g_final = 255*((100-g_column)/100);
-  int b_final = 255*((100-b_column)/100);
-
-  r_g_b[0] = r_final;
-  r_g_b[1] = g_final;
-  r_g_b[2] = b_final;
-  
+  r_g_b[0] = (r_steps/max_steps)*255;
+  r_g_b[1] = (g_steps/max_steps)*255;
+  r_g_b[2] = (b_steps/max_steps)*255;
 }
 
 void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
